450d1cf396e7fbbe788a21cb89d7467011ae0d9e
PekMela <92750982+PekMela@users.noreply.github.com>
2023-02-28 04:58:46 +0200

    Initial commit

---
+++ hehe.js
@@ -0,0 +1,186 @@
+function interesantifact() {
+    const container = document.getElementById('azif');
+
+if (container.textContent.includes('Azirafals')) {
+  var myarray= new Array("Good Omens jau iepriekš tika pielāgots kā radio adaptācija","Terijs Giljams ilgu laiku strādāja pie filmas Good Omens adaptācijas.","Džonijs Deps un Robins Viljamss gandrīz filmējās filmā Good Omens.","Bija gandrīz filmas Good Omens versija, kas būtu ļoti atšķirīga no grāmatas.","Good Omens tika nominēta reliģiskās fantastikas balvai.","Viens intervētājs nesaprata, ka Good Omens ir daiļliteratūra.","Amerikāņu izdevums Good Omens sākotnēji bija garāks.","Pratchett un Geiman sadarbojās Good Omens, nosūtot disketes šurpu turpu viens otram.","Maikls Šīns ir milzīgs grāmatas Labās zīmes fans","Lielākā daļa dziesmu Good Omens ir Queen","Nīlam Geimenam ir epizode ceturtajā sērijā","Tās pilnais nosaukums ir: Labas zīmes: Agnesas Nuteres, raganas jaukie un precīzie pareģojumi.","Pirmo reizi publicēts 1990.","Autori Nīls Geimans un sers Terijs Pračets.","Kopējais aktieru skaits ir 37");
+    var random = myarray[Math.floor(Math.random() * myarray.length)];
+    alert(random)
+} if (container.textContent.includes('Aziraphale')){
+    var myarray= new Array("Good Omens has been adapted before as a radio  adaptation","Terry Gilliam was working on a movie adaptation of Good Omens for ages.","Johnny Depp and Robin Williams almost starred in the Good Omens movie.","There was almost a film version of Good Omens that would have been very different from the book.","Good Omens was nominated for a religious fiction award.","One interviewer didn't realize Good Omens was fiction.","The American edition of Good Omens was originally longer.","Pratchett and Gaiman collaborated on Good Omens by mailing floppy disks back and forth to one another.","Michael Sheen is a huge fan of the Good Omens book","Most songs in Good Omens are by Queen","Neil Gaiman has a cameo in episode four","Its full title is: Good Omens: The Nice and Accurate Prophecies of Agnes Nutter, Witch.","First published in 1990.","Written by Neil Gaiman and Sir Terry Pratchett.","The total number of actors is 37 (not including the 11 cameos)");
+    var random = myarray[Math.floor(Math.random() * myarray.length)];
+    alert(random)
+} if (container.textContent.includes('Erziraphael')){
+    var myarray= new Array("item 2","koks");
+    var random = myarray[Math.floor(Math.random() * myarray.length)];
+    alert(random)
+}
+}
+
+function flag1(){
+    document.getElementById("ievads").innerHTML = "Introduction";
+    document.getElementById("subievads").innerHTML = "A brief description of the series";
+    document.getElementById("ievadstext").innerHTML = 'Based on the book, this fantasy series sees the edgy angel Aziraphale and the free-spirited demon Crowley team up to form an unlikely duo. Both are too fond of life on Earth and are forced to work together to stop the impending "Armageddon".';
+    document.getElementById('nosua').innerHTML = "Name";
+    document.getElementById('time').innerHTML = "Time";
+    document.getElementById('imdb').innerHTML = "IMDB rating";
+    document.getElementById('desc').innerHTML = "Short description";
+    document.getElementById('desc1').innerHTML = "In the beginning and eleven years ago, two immortal beings decide that it might not be <br> time to start an Apocalypse.";
+    document.getElementById('desc2').innerHTML = "Having followed the wrong boy for years, Aziraphale and Crowley must now try to locate <br> the whereabouts of real Antichrist.";
+    document.getElementById('desc3').innerHTML = "Agnes Nutter's descendant Anathema arrives in Tadfield on her own mission to save the world.";
+    document.getElementById('desc4').innerHTML = "Armageddon starts in earnest, with the Antichrist's powers wreaking havoc across the globe.";
+    document.getElementById('desc5').innerHTML = "Aziraphale and Crowley race towards Tadfield airbase as they attempt to prevent Adam and the <br> Four Horsemen from beginning the apocalypse.";
+    document.getElementById('desc6').innerHTML = "Can Adam, Crowley and Aziraphale work together to fight the powers of Heaven and Hell <br> and prevent the apocalypse?";
+    document.getElementById('qoute1').innerHTML = 'Aziraphale quote';
+    document.getElementById('qoute12').innerHTML = '"He ought to tell Crowley. No, he didn’t. He wanted to tell Crowley. He ought to tell Heaven."';
+    document.getElementById('qoute2').innerHTML = 'Crowley quote';
+    document.getElementById('qoute22').innerHTML = '"A demon can get into real trouble doing the right thing. Funny if we both got it wrong, eh? Funny if I did the good thing, and you did the bad one, eh?"';
+    document.getElementById('qoute3').innerHTML = 'Death quote';
+    document.getElementById('qoute32').innerHTML = '"Don’t think of it as dying. Just think of it as leaving early to avoid the rush."';
+    document.getElementById('qoute4').innerHTML = 'Crowley quote';
+    document.getElementById('qoute42').innerHTML = '"What are they putting in bananas these days?"';
+    document.getElementById('qoute5').innerHTML = 'Adam Young quote';
+    document.getElementById('qoute52').innerHTML = '"I don’t see what’s so triflic about creating people as people and then getting upset cause they act like people."';
+    document.getElementById('qoute6').innerHTML = 'Aziraphale quote';
+    document.getElementById('qoute62').innerHTML = '"You can’t kill me. There will be paperwork."';
+    document.getElementById('qoute7').innerHTML = 'Narrator quote';
+    document.getElementById('qoute72').innerHTML = 'Many people, meeting Aziraphale for the first time, formed three impressions: that he was English, that he was intelligent, and that he was gayer than a treeful of monkeys on nitrous oxide.';
+    document.getElementById('int').innerHTML = "Interesting facts!";
+    document.getElementById("azif").innerHTML = "Aziraphale";
+    document.getElementById("azift").innerHTML = "Aziraphale is one of the main characters in Amazon series Good Omens. He is an angel who doubles as a rare book dealer and has fallen in love with human food.";
+    document.getElementById("cro").innerHTML = "Crowley";
+    document.getElementById("crot").innerHTML = "Crowley is one of the main characters in Amazon series Good Omens. He is a demon who did not so much fall from Heaven as Saunter Vaguely Downwards.";
+    document.getElementById("per1").innerHTML = "Adam Young";
+    document.getElementById("per2").innerHTML = "Anathema Device";
+    document.getElementById("per3").innerHTML = "Pollution";
+    document.getElementById("per4").innerHTML = "Death";
+    document.getElementById("per5").innerHTML = "Famine";
+    document.getElementById("per6").innerHTML = "Witchfinder Pulsifer";
+    document.getElementById("per7").innerHTML = "Newton Pulsifer";
+    document.getElementById("per8").innerHTML = "Agnes Nutter";
+    document.getElementById("per9").innerHTML = "Pepper";
+    document.getElementById("per10").innerHTML = "Brian";
+    document.getElementById("per11").innerHTML = "Wensleydale";
+    document.getElementById("per12").innerHTML = "Dog";
+    document.getElementById("per13").innerHTML = "Gabriel";
+    document.getElementById("per14").innerHTML = "Beelzebub";
+    document.getElementById("per15").innerHTML = "War";
+    document.getElementById("per16").innerHTML = "Satan";
+    document.getElementById("per17").innerHTML = "Arthur Young";
+    document.getElementById("per18").innerHTML = "Madame Tracy";
+    document.getElementById("li1").innerHTML = "Stellar Cast";
+    document.getElementById("li2").innerHTML = "It’s a Miniseries";
+    document.getElementById("li3").innerHTML = "A Stellar Adaptation";
+    document.getElementById("li4").innerHTML = "Intriguing Plot";
+    document.getElementById("why").innerHTML = "Wht you should watch it?";
+    document.getElementById("siml").innerHTML = "Similar TV shows";
+    document.getElementById("up").innerHTML = "To top";
+    document.getElementById("cool").innerHTML = "you are cool";
+    document.getElementById('saturs').innerHTML = "Content";
+    document.getElementById('goo1').innerHTML = "Introduction";
+    document.getElementById('goo2').innerHTML = "Episodes";
+    document.getElementById('goo3').innerHTML = "Quots";
+    document.getElementById('goo4').innerHTML = "Main characters";
+    document.getElementById('goo5').innerHTML = "Rest of the characters";
+    document.getElementById('goo6').innerHTML = "More";
+}  
+
+function flag3(){
+    document.getElementById("ievads").innerHTML = "Ievads";
+    document.getElementById("subievads").innerHTML = "Īss seriāla apraksts";
+    document.getElementById("ievadstext").innerHTML = 'Šajā fantāzijas sērijālā, kas ir balstīta uz grāmatu, tiek parādīts kā nervozais eņģelis Azirafals un brīvs dēmons Kroulijs sadarbojas, lai izveidotu maz ticamu duetu. Abi ir pārāk iecienījuši dzīvi uz Zemes, un viņi ir spiesti sadarboties, cenšoties apturēt tuvojošos "Armagedonu".';
+    document.getElementById('nosua').innerHTML = "Nosaukums";
+    document.getElementById('time').innerHTML = "Ilgums";
+    document.getElementById('imdb').innerHTML = "IMDB vērtējums";
+    document.getElementById('desc').innerHTML = "Īss apraksts";
+    document.getElementById('desc1').innerHTML = "Sākumā un pirms vienpadsmit gadiem divas nemirstīgas būtnes nolemj, ka varbūt nav <br> laika sākt Apokalipses gaitas.";
+    document.getElementById('desc2').innerHTML = "Gadiem ilgi sekojis nepareizajam zēnam, Azirafalam un Kroulijam tagad jāmēģina atrast īstās<br> Antikrista atrašanās vietu.";
+    document.getElementById('desc3').innerHTML = "Agneses Nateres pēctece Anatēma ierodas Tadfieldā uz savu misiju, lai glābtu pasauli.";
+    document.getElementById('desc4').innerHTML = '“Armagedons” sākas, kad antikrista spējas rada nekārtību visā pasaulē. ';
+    document.getElementById('desc5').innerHTML = "Azirafals un Kroulijs joņo uz Tadfield gaisa spēku bāzi, mēģinādami novērst to,<br> ka Ādams un četri jātnieki sāk apokalipsi.";
+    document.getElementById('desc6').innerHTML = "Vai Ādams, Kroulijs un Azirafals var sadarboties, lai cīnītos pret Debesu un Elles varenību un<br> novērstu apokalipsi?";
+    document.getElementById('qoute1').innerHTML = 'Azirafala teiktais';
+    document.getElementById('qoute12').innerHTML = '"Viņam vajadzētu pastāstīt Kroulijam. Nē. Viņš gribēja pastāstīt Kroulijam. Viņam būtu jāpastāsta Debesīm."';
+    document.getElementById('qoute2').innerHTML = 'Kroulija teiktais';
+    document.getElementById('qoute22').innerHTML = '“Dēmonam var rasties problēmas, rīkojoties pareizi. Smieklīgi, ja mēs abi kļūdījāmies, vai ne? Smieklīgi, ja es izdarīju labo lietu, bet tu - slikto, vai ne?"';
+    document.getElementById('qoute3').innerHTML = 'Nāves teiktais';
+    document.getElementById('qoute32').innerHTML = '"Nedomājiet par to kā par nāvi. Vienkārši iedomājieties to kā agru izbraukšanu, lai izvairītos nosteigas."';
+    document.getElementById('qoute4').innerHTML = 'Kroulija teiktais';
+    document.getElementById('qoute42').innerHTML = '"Ko viņi šajās dienās liek banānos?"';
+    document.getElementById('qoute5').innerHTML = 'Ādama Jungu teiktais';
+    document.getElementById('qoute52').innerHTML = '"Es nesaprotu, kas ir tik mazsvarīgs tajā, ka cilvēkus veido kā cilvēkus un pēc tam tiek sarūgtināts, jo viņi uzvedas kā cilvēki."';
+    document.getElementById('qoute6').innerHTML = 'Azirafals';
+    document.getElementById('qoute62').innerHTML = '"Tu nevari mani nogalināt. Būs jāpilda papīri."';
+    document.getElementById('qoute7').innerHTML = 'Stāstītāja teiktais';
+    document.getElementById('qoute72').innerHTML = 'Daudziem cilvēkiem, pirmo reizi satiekot Azirafalu, rodas trīs iespai: ka viņš ir anglis, ka viņš ir inteliģents un ka viņš ir gejāks par koku pilnu ar pērtiķiem uz slāpekļa oksīda.';
+    document.getElementById('int').innerHTML = "Interesanti fakti!";
+    document.getElementById("azif").innerHTML = "Azirafals";
+    document.getElementById("azift").innerHTML = "Azirafals ir viens no galvenajiem varoņiem Amazon seriālā Good Omens. Viņš ir eņģelis, kurš darbojas kā rets grāmatu tirgotājs, un ir iemīlējis cilvēku ēdienu.";
+    document.getElementById("cro").innerHTML = "Kroulijs";
+    document.getElementById("crot").innerHTML = "Kroulijs ir viens no galvenajiem varoņiem Amazon seriālā Good Omens. Viņš ir dēmons, kurš ne tik daudz krita no debesīm, cik noslīdēja neskaidri lejup.";
+    document.getElementById("per1").innerHTML = "Ādams Jangs";
+    document.getElementById("per2").innerHTML = "Anatēma Divaisa";
+    document.getElementById("per3").innerHTML = "Piesārņojums";
+    document.getElementById("per4").innerHTML = "Nāve";
+    document.getElementById("per5").innerHTML = "Bads";
+    document.getElementById("per6").innerHTML = "Sers Pulsifers";
+    document.getElementById("per7").innerHTML = "Ņūtons Pulsifers";
+    document.getElementById("per8").innerHTML = "Agnese Natere";
+    document.getElementById("per9").innerHTML = "Pipars";
+    document.getElementById("per10").innerHTML = "Braiens";
+    document.getElementById("per11").innerHTML = "Venslideils";
+    document.getElementById("per12").innerHTML = "Suns";
+    document.getElementById("per13").innerHTML = "Gabriels";
+    document.getElementById("per14").innerHTML = "Belcebuls";
+    document.getElementById("per15").innerHTML = "Karš";
+    document.getElementById("per16").innerHTML = "Sātans";
+    document.getElementById("per17").innerHTML = "Artūrs Jungsa";
+    document.getElementById("per18").innerHTML = "Misis Treisija";
+    document.getElementById("li1").innerHTML = "Brīnišķīgi aktieri";
+    document.getElementById("li2").innerHTML = "Tas ir miniseriāls";
+    document.getElementById("li3").innerHTML = "Ļoti laba adaptācija";
+    document.getElementById("li4").innerHTML = "Intriģējošs sižets";
+    document.getElementById("why").innerHTML = "Kapēc sktīties?";
+    document.getElementById("siml").innerHTML = "Līdzīgi seriāli";
+    document.getElementById("up").innerHTML = "Uz augšūuu";
+    document.getElementById("cool").innerHTML = "tu esi foršs";
+    document.getElementById('saturs').innerHTML = "Saturs";
+    document.getElementById('goo1').innerHTML = "Ievads";
+    document.getElementById('goo2').innerHTML = "Sērijas";
+    document.getElementById('goo3').innerHTML = "Atsauces";
+    document.getElementById('goo4').innerHTML = "Galvenie varoņi";
+    document.getElementById('goo5').innerHTML = "Pārējie varoņi";
+    document.getElementById('goo6').innerHTML = "Vel";
+}  
+
+function topp(){
+    document.querySelector('#saturs').scrollIntoView();
+}
+
+function go1(){
+    document.querySelector('#ievads').scrollIntoView();
+}
+
+function go2(){
+    document.querySelector('#tabula').scrollIntoView();
+}
+
+function go3(){
+    document.querySelector('#qoute1').scrollIntoView();
+}
+
+function go4(){
+    document.querySelector('#azif').scrollIntoView();
+}
+
+function go5(){
+    document.querySelector('#per1').scrollIntoView();
+}
+
+function go6(){
+    document.querySelector('#why').scrollIntoView();
+}
+
+function myFunction() {
+    var element = document.body;
+    element.classList.toggle("dark");
+    }
